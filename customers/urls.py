e7ea90c17a40af50a4f6b8604e6892a538ace9de
sohail70 <sohail.e.nia@gmail.com>
2022-11-29 18:52:31 +0330

    Add out first get endpoint

--- customers/urls.py
+++ customers/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from customers import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/customers/',views.customers , name='customers')
+    path('api/customers/',views.customers , name='customers') 
 ]
