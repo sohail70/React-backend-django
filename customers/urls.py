ac83bdc77ad5d0c1256f0f9afd04be6d77cf331f
sohail70 <sohail.e.nia@gmail.com>
2022-11-29 20:16:13 +0330

    Create a new endpoints to reach each customer individually

--- customers/urls.py
+++ customers/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from customers import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/customers/',views.customers , name='customers') 
+    path('api/customers/',views.customers , name='customers') ,
+    path('api/customers/<int:id>', views.customer , name='customer')
 ]
--- customers/views.py
+++ customers/views.py
@@ -8,3 +8,9 @@ def customers(request):
     # now serialize all this data
     serializer = CustomerSerializer(data , many=True)
     return JsonResponse({'customers': serializer.data}) #serializer.data dar vaghe serialize shode ye data  hast- choon in ba json compatible hast va ma ino be onvan response miferestim
+
+def customer(request,id):
+    
+    data = Customer.objects.get(pk=id) # we don't wanna get all of them so we use get() instead of all() 
+    serializer = CustomerSerializer(data)
+    return JsonResponse({'customer': serializer.data}) #serializer.data dar vaghe serialize shode ye data  hast- choon in ba json compatible hast va ma ino be onvan response miferestim
