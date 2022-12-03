654643770a38e659c253200c6b698b5c70b83b9f
sohail70 <sohail.e.nia@gmail.com>
2022-12-03 20:29:05 +0330

    Add 404 try except to view.py

--- customers/serializers.py
+++ customers/serializers.py
@@ -6,3 +6,4 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer #model e ke mikhay serialize kunim
         fields = '__all__' #che field hayee tosh mikhaym? hame --> mishe har attribute ke mikhay ro to list bezari
+        
--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,5 @@
 from customers.models import Customer
-from django.http import JsonResponse
+from django.http import JsonResponse , Http404
 from customers.serializers import CustomerSerializer
 
 def customers(request):
@@ -10,7 +10,10 @@ def customers(request):
     return JsonResponse({'customers': serializer.data}) #serializer.data dar vaghe serialize shode ye data  hast- choon in ba json compatible hast va ma ino be onvan response miferestim
 
 def customer(request,id):
-    
-    data = Customer.objects.get(pk=id) # we don't wanna get all of them so we use get() instead of all() 
+    try:
+        data = Customer.objects.get(pk=id) # we don't wanna get all of them so we use get() instead of all() 
+    except Customer.DoesNotExist:
+        raise Http404('Customer does not exist') 
+
     serializer = CustomerSerializer(data)
     return JsonResponse({'customer': serializer.data}) #serializer.data dar vaghe serialize shode ye data  hast- choon in ba json compatible hast va ma ino be onvan response miferestim
